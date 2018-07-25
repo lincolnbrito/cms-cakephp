9741ef5a21a57d801acbf819522dcb6ffe9b3d9f
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-25 19:11:41 -0300

    add select field to set tags on articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -34,13 +34,21 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article'));
         }
+        // get a list of tags
+        $tags = $this->Articles->Tags->find('list');
+
+        //set tags to the view context
+        $this->set('tags', $tags);
 
         $this->set('article', $article);
     }
 
     public function edit($slug)
     {
-        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->contain('Tags') //load associated tags
+            ->firstOrFail();
 
         if($this->request->is(['post','put'])) {
             $this->Articles->patchEntity($article, $this->request->getData());
@@ -50,6 +58,10 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to update your article.'));
         }
+        //get a list of tags
+        $tags = $this->Articles->Tags->find('list');
+
+        $this->set('tags', $tags);
 
         $this->set('article', $article);
     }

beac2014bb49781c5342626c8da6da8dfa0ba59a
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-28 19:20:54 -0300

    configure Auth component

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -36,6 +36,7 @@ class AppController extends Controller
      * e.g. `$this->loadComponent('Security');`
      *
      * @return void
+     * @throws \Exception
      */
     public function initialize()
     {
@@ -51,5 +52,26 @@ class AppController extends Controller
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
          */
         //$this->loadComponent('Security');
+
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            //use isAuthorized in Controllers
+            'authorize' => ['Controller'],
+            //if unauthorized, return to page they were just on
+            'unanuthorizedRedirect' => $this->referer()
+        ]);
+
+        $this->Auth->allow(['display','view','index']);
     }
 }

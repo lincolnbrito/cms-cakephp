703eaa455e1f1464f718e5910825e28097ce3e0f
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-08-01 20:25:16 -0300

    add contactform

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -16,7 +16,7 @@ class UsersController extends AppController
     public function initialize()
     {
         parent::initialize();
-        $this->Auth->allow(['logout']);
+        $this->Auth->allow(['logout','add']);
     }
 
     /**
